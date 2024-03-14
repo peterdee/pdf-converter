b6cd648958360961328e9726280a024e4f6b4114
Peter <peter.dyumin@gmail.com>
2024-03-14 22:57:50 +0300

    Add transaction for Queue file handler

--- converter/handlers/queue-file.go
+++ converter/handlers/queue-file.go
@@ -3,12 +3,16 @@ package handlers
 import (
 	"context"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/julyskies/gohelpers"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 
 	"converter/constants"
 	"converter/database"
@@ -34,38 +38,61 @@ func QueueFile(encoded string, filename string) (*QueueFileResult, error) {
 	}
 	os.WriteFile(fmt.Sprintf("%s/%s.pdf", dirPath, hash), bytes, 0777)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	now := gohelpers.MakeTimestamp()
-	_, insertionError := database.Queue.InsertOne(ctx, bson.D{
-		{Key: "createdAt", Value: now},
-		{Key: "downloadedAt", Value: now},
-		{Key: "originalFileName", Value: filename},
-		{Key: "status", Value: constants.QUEUE_STATUSES.Queued},
-		{Key: "totalDownloads", Value: 0},
-		{Key: "uid", Value: hash},
-		{Key: "updatedAt", Value: now},
-	})
-	if insertionError != nil {
-		return nil, insertionError
+	session, sessionError := database.Client.StartSession()
+	if sessionError != nil {
+		return nil, sessionError
 	}
+	defer session.EndSession(ctx)
 
-	count, countError := database.Queue.CountDocuments(
+	result, transactionError := session.WithTransaction(
 		ctx,
-		bson.D{
-			{Key: "status", Value: constants.QUEUE_STATUSES.Queued},
-		},
-	)
-	if countError != nil {
-		return nil, countError
-	}
+		func(tctx mongo.SessionContext) (interface{}, error) {
+			now := gohelpers.MakeTimestamp()
+			_, insertionError := database.Queue.InsertOne(
+				tctx,
+				bson.D{
+					{Key: "createdAt", Value: now},
+					{Key: "downloadedAt", Value: now},
+					{Key: "originalFileName", Value: filename},
+					{Key: "status", Value: constants.QUEUE_STATUSES.Queued},
+					{Key: "totalDownloads", Value: 0},
+					{Key: "uid", Value: hash},
+					{Key: "updatedAt", Value: now},
+				},
+			)
+			if insertionError != nil {
+				return nil, insertionError
+			}
 
-	var queueResult = &QueueFileResult{
-		Filename:    filename,
-		QueuedItems: count,
-		Status:      constants.QUEUE_STATUSES.Queued,
-		UID:         hash,
+			count, countError := database.Queue.CountDocuments(
+				tctx,
+				bson.D{
+					{Key: "status", Value: constants.QUEUE_STATUSES.Queued},
+				},
+			)
+			if countError != nil {
+				return nil, countError
+			}
+
+			var queueResult = &QueueFileResult{
+				Filename:    filename,
+				QueuedItems: count,
+				Status:      constants.QUEUE_STATUSES.Queued,
+				UID:         hash,
+			}
+			return queueResult, nil
+		},
+		options.Transaction().SetWriteConcern(writeconcern.Majority()),
+	)
+	if transactionError != nil {
+		return nil, transactionError
 	}
-	return queueResult, nil
+	if converted, ok := result.(*QueueFileResult); ok {
+		return converted, nil
+	} else {
+		return nil, errors.New(constants.ERROR_MESSAGES.FailedToConvertData)
+	}
 }
